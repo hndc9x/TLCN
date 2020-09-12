61dc265ee21bf948d33b079bf61955534dd0dfa5
hndc9x <hndc9x@users.noreply.github.com>
2020-09-12 15:53:54 +0700

    update

---
+++ API/Models/CategoryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace API.Models
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext(DbContextOptions<CategoryContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Category> categogies { get; set; }
+    }
+}
